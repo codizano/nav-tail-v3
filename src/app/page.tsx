a48912965b28956fea43c7de678b4f03fd02b08e
Ricardo Otálora Luna <ruperto1@protonmail.com>
2024-03-03 15:40:58 -0500

    footer section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 import Hero from "@components/Hero";
-import { Navbar } from "@components/Navbar";
-import Blogs from "@components/Blog/Blogs";
+import Navbar from "@components/Navbar";
+import Blogs from "@components/Blog";
+import Footer from "@components/Footer";
+
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Hero />
       <Blogs />
+      <Footer />
     </div>
   );
 }
